9cf1c2405fa507f0ac02f84a1c23f7006d71efc3
hyojoo <jhj080802@naver.com>
2024-08-28 23:48:36 +0900

    console

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,16 @@
-import { resolve } from "path";
+// import { resolve } from "path";
 import { API_URL } from "../../../(home)/page";
 
 async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
@@ -18,6 +20,9 @@ export default async function MovieDetail({
 }: {
   params: { id: string };
 }) {
+  console.log("===========");
+  console.log("start fetching");
   const [movie, videos]=await Promise.all([getMovie(id), getVideos(id);])
+  console.log("end fetching");
   return <h1>{movie.title}</h1>;
 }
