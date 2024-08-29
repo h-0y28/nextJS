79bc944457f5b7283cf5717b58c6866f3997c647
hyojoo <jhj080802@naver.com>
2024-08-29 22:54:10 +0900

    fixed error - movie styles

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,11 +1,11 @@
-// import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/(home)/page";
 
 async function getVideo(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("Something broke...");
-  // const response = await fetch(`${API_URL}/${id}`);
-  // return response.json();
+  // console.log(`Fetching movies: ${Date.now()}`);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("Something broke...");
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
 }
 
 export default async function MoiveVideos({ id }: { id: string }) {
