6072c8cd1bd3edae515e3f77348da2708f075573
hyojoo <jhj080802@naver.com>
2024-08-29 20:25:58 +0900

    global css - incomplete

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,8 +3,9 @@ import { API_URL } from "../app/(home)/page";
 async function getVideo(id: string) {
   console.log(`Fetching movies: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
+  throw new Error("Something broke...");
+  // const response = await fetch(`${API_URL}/${id}`);
+  // return response.json();
 }
 
 export default async function MoiveVideos({ id }: { id: string }) {
