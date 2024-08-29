59f057f9bc25f42331ebd508fd035254c431c681
hyojoo <jhj080802@naver.com>
2024-08-29 11:28:14 +0900

    Suspense h4=경계

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -11,9 +11,11 @@ export default async function MovieDetail({
 }) {
   return (
     <div>
+      <h4>Movie detail page</h4>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MoiveInfo id={id} />
       </Suspense>
+      <h4>Videos</h4>
       <Suspense fallback={<h1>Loading movie video</h1>}>
         <MoiveVideos id={id} />
       </Suspense>
