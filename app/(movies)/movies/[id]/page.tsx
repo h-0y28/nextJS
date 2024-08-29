ba6ecc65c5848bb7b714c6276556ad1405205678
hyojoo <jhj080802@naver.com>
2024-08-30 00:02:31 +0900

    Movie Trailers

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,6 @@
 // import { resolve } from "path";
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
+// import { API_URL } from "../../../(home)/page";
 import MoiveInfo from "../../../../components/movie-info";
 import MoiveVideos from "../../../../components/movie-videos";
 
@@ -11,11 +11,9 @@ export default async function MovieDetail({
 }) {
   return (
     <div>
-      <h4>Movie detail page</h4>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MoiveInfo id={id} />
       </Suspense>
-      <h4>Videos</h4>
       <Suspense fallback={<h1>Loading movie video</h1>}>
         <MoiveVideos id={id} />
       </Suspense>
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,14 +1,26 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideo(id: string) {
-  // console.log(`Fetching movies: ${Date.now()}`);
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  // throw new Error("Something broke...");
-  const response = await fetch(`${API_URL}/${id}`);
+  new Error("Something broke...");
+  const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
 export default async function MoiveVideos({ id }: { id: string }) {
   const videos = await getVideo(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          className={styles.iframe}
+          key={video.id}
+          src={`http://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
