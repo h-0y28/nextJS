0789d4c36395e8f7ec69eab745b45772222b2162
hyojoo <jhj080802@naver.com>
2024-08-30 12:17:48 +0900

    deployment

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,22 @@
 // import { resolve } from "path";
 import { Suspense } from "react";
 // import { API_URL } from "../../../(home)/page";
-import MoiveInfo from "../../../../components/movie-info";
+import MoiveInfo, { getMovie } from "../../../../components/movie-info";
 import MoiveVideos from "../../../../components/movie-videos";
+import { title } from "process";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
